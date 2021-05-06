c0820a7196781a4619f5d889a135e68a02695a8b
Nicola <nicspanu@gmail.com>
2021-05-06 10:44:01 +0200

    'hypothesis'

---
+++ contact_book_app/tests/test_properties.py
@@ -0,0 +1,17 @@
+from hypothesis import given
+import hypothesis.strategies as st
+
+from contacts import Application
+
+
+@given(st.text())
+def test_adding_contacts(name):
+    app = Application()
+
+    app.run(f"contacts add {name} 3456789")
+
+    name = name.strip()
+    if name:
+        assert app._contacts == [(name, "3456789")]
+    else:
+        assert app._contacts == []
