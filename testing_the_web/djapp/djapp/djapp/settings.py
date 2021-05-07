7d9a263dd5b837786038880f02c808b45b0eaf0e
Nicola <nicspanu@gmail.com>
2021-05-07 12:39:19 +0200

    'django'

--- testing_the_web/djapp/djapp/djapp/settings.py
+++ testing_the_web/djapp/djapp/djapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zwvol69gg#$mnhmx&8cq&tn+xe42q64j+s^(5vy_%&8v!jvwug
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["httpbin.org", "127.0.0.1"]
 
 
 # Application definition
@@ -43,7 +43,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
